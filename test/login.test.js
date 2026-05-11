0ad9e761727c87b2ac2f3611662394622cddd163
jose.bianchini <jose.bianchini@siplan.com>
2026-05-11 19:48:16 -0300

    primeiro teste de API

---
+++ test/login.test.js
@@ -0,0 +1,21 @@
+const request = require('supertest');
+const { expect } = require('chai');
+
+describe('Login', () => {
+    describe('POST /login', () => {
+        it('deve retornar 200 com um token string quando usar credenciais válidas', async () => {
+            const res = await request('http://localhost:3000')
+                .post('/login')
+                .set('Content-Type', 'application/json')
+                .send({
+                    "username": "usuario",
+                    "senha": "123456"
+                });
+            console.log(res.statusCode);
+            console.log(res.body);
+
+            expect(res.statusCode).to.equal(200);
+            expect(res.body.token).to.be.a('string');
+        });
+    });
+});
